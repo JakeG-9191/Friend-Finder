a6b568dee9701e982999e2d534f6dd4a5f04a07a
Jake Garlick <jacobgarlick@msn.com>
2019-10-13 21:41:11 -0600

    built out all questions for the survey, stated to build out logic for storing answers to compare friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,9 @@
+var friends = require("../data/friends");
 
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+      res.json(friends);
+    });
 
 app.get("/api/friends", function (req, res){
     return res.json(friends)
