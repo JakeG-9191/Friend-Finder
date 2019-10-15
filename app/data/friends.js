f6db6b318eaec954705f98e01562d5c180a88d54
Jake Garlick <jacobgarlick@msn.com>
2019-10-14 19:01:44 -0600

    continued to work on logic affecting how friends are decided

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friends = [
     {
         "name": "Ahmed",
         "photo": "https://media.istockphoto.com/photos/portrait-of-smiling-handsome-man-in-blue-tshirt-standing-with-crossed-picture-id1045886560?k=6&m=1045886560&s=612x612&w=0&h=hXrxai1QKrfdqWdORI4TZ-M0ceCVakt4o6532vHaS3I=",
-        "scores": [
+        "userScore": [
             5,
             1,
             4,
@@ -18,7 +18,7 @@ var friends = [
     {
         "name": "Johnson",
         "photo": "https://media.istockphoto.com/photos/not-sure-young-adult-businessman-have-a-doubt-picture-id836798276?k=6&m=836798276&s=612x612&w=0&h=RgusLf2oTOHlq4nv1D-EwW6Ld6e4fpicnfylvJT5Ey4=",
-        "scores": [
+        "userScore": [
             5,
             1,
             4,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 var friends = require("../data/friends");
 
-module.exports = function (app) {
+module.exports = function(app) {
 
     app.get("/api/friends", function (req, res) {
         res.json(friends)
@@ -13,24 +13,14 @@ module.exports = function (app) {
 
         for (var i = 0; i < newUser.userScore.length; i++) {
             newUser.userScore[i] = parseInt(newUser.userScore[i]);
+            console.log(newUser.userScore[i])
         };
 
         var friendMatch = 0;
         var friendDefault = 50;
 
-        // for (var i = 0; i < friends.length; i++) {
-        //     var friendDifference = 0;
-        //     for (var x = 0; x < friends[i].userScore.length; x++) {
-        //         var difference = Math.abs(newUser.userScore[x] - friends[i].userScore[x]);
-        //         friendDifference += difference;
-        //     };
+        
 
-
-        //     if (friendDifference < friendDefault) {
-        //         friendMatch = i;
-        //         friendDefault = friendDifference;
-        //     };
-        // };
         friends.push(newUser)
 
         res.json(friends[friendMatch])
