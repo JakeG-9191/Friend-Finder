eb67c1ba56e42f4819ed97b7458831fe21c20301
Jake Garlick <jacobgarlick@msn.com>
2019-10-14 18:04:19 -0600

    built in ability to push new scores to friends array after they complete the survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,30 +7,30 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        console.log(req.body.scores)
+        console.log(req.body.userScore)
 
         var newUser = req.body;
 
-        for (var i = 0; i < newUser.score.length; i++) {
-            newUser.scores[i] = parseInt(newUser.score[i]);
+        for (var i = 0; i < newUser.userScore.length; i++) {
+            newUser.userScore[i] = parseInt(newUser.userScore[i]);
         };
 
         var friendMatch = 0;
         var friendDefault = 50;
 
-        for (var i = 0; i < friends.length; i++) {
-            var friendDifference = 0;
-            for (var x = 0; x < friends[i].scores.length; x++) {
-                var difference = Math.abs(newUser.scores[x] - friends[i].scores[x]);
-                friendDifference += difference;
-            };
+        // for (var i = 0; i < friends.length; i++) {
+        //     var friendDifference = 0;
+        //     for (var x = 0; x < friends[i].userScore.length; x++) {
+        //         var difference = Math.abs(newUser.userScore[x] - friends[i].userScore[x]);
+        //         friendDifference += difference;
+        //     };
 
 
-            if (friendDifference < friendDefault) {
-                friendMatch = i;
-                friendDefault = friendDifference;
-            };
-        };
+        //     if (friendDifference < friendDefault) {
+        //         friendMatch = i;
+        //         friendDefault = friendDifference;
+        //     };
+        // };
         friends.push(newUser)
 
         res.json(friends[friendMatch])
